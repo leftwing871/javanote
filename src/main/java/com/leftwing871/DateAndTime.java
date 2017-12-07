42e76c5259ae08fdb26e165c48d16ebb267c309f
leftwing871 <leftwing871@gmail.com>
2017-12-07 10:59:52 +0900

    Initial commit

---
+++ src/main/java/com/leftwing871/DateAndTime.java
@@ -0,0 +1,19 @@
+package com.leftwing871;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class DateAndTime {
+
+    public static void main(String[] args) {
+        //1. Calendar를 Date로 변환
+        Calendar cal = Calendar.getInstance();
+        Date d = new Date(cal.getTimeInMillis());   //Date(long date)
+
+        //2.Date를 Calendar로 변환
+        Date dd = new Date();
+        Calendar calcal = Calendar.getInstance();
+        cal.setTime(dd);
+    }
+
+}
